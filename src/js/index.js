082e212f624e42245fb51396e08cf1bf28ce3f72
SergeyKorol1982 <giyuki@gmail.com>
2024-08-12 12:13:36 +0300

    testimonials block added

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,21 @@ var swiper = new Swiper(".services__swiper", {
     },
 },
 });
+
+var swiper1 = new Swiper(".testimonials__swiper", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  centeredSlides: false,
+    navigation: {
+      nextEl: ".testimonials__next",
+      prevEl: ".testimonials__prev",
+    },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2.5,
+      spaceBetween: 50,
+      initialSlide: 0,
+    },
+},
+});
